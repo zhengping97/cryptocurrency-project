e01ab2c7d663e29a2b7806d9293e9817e7597355
Heng Zheng Ping <36413759+zhengping97@users.noreply.github.com>
2022-11-15 11:23:49 +0800

    Update AboutPage.js

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,7 +4,7 @@ function AboutPage() {
             <h1>ABOUT</h1>
             <p class="fst-normal col-md-5 mx-auto mt-5">This web application tracks the real-time price and data of XRP. In addition,
                 it calculates the possible price which XRP can reached, using the market cap of the 7 other coins that
-                ranks above XRP as of 21 Dec 2022. Besides that, this application also displays 3 other potential coins,
+                ranks above XRP as of 21 Dec 2021. Besides that, this application also displays 3 other potential coins,
                 theGraph, Hedera Hashgraph as well as the Sandbox with it's expected price predicted using market cap of XRP.</p>
         </div>
     )
